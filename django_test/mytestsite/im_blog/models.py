f3fdf211ff9051ab996d8156736e02f60d38f787
Kwongmin Lim <63140834+dnwjd6452@users.noreply.github.com>
2021-03-24 16:45:58 +0900

    Update models.py

--- django_test/mytestsite/im_blog/models.py
+++ django_test/mytestsite/im_blog/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    authol = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_data = models.DateTimeField(default=timezone.now)
-    published_data = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_data = timezone.now()
