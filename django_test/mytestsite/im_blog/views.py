67137d77d43d0d9ad43583ef8e9293c1366989d6
Kwongmin Lim <63140834+dnwjd6452@users.noreply.github.com>
2021-03-24 16:45:29 +0900

    Update views.py

--- django_test/mytestsite/im_blog/views.py
+++ django_test/mytestsite/im_blog/views.py
@@ -4,8 +4,10 @@ from django.utils import timezone
 from .models import Post
 
 
+
 def post_list(request):
-    return render(request,'im_blog/post_list.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'im_blog/post_list.html',{'posts':posts})
 
 def create(request):
     blog = Post()
